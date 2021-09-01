a7db98711c2fda7f556e275a6e9643357dd63665
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-09-01 15:59:55 +0300

    Added ChatForm Component that transmits messages received input

---
+++ client/src/components/ChatForm.js
@@ -0,0 +1,28 @@
+import styles from './styles.module.css';
+import { useState } from 'react';
+import { sendMessage } from '../socketApi';
+import { useChat } from '../context/ChatContext';
+
+function ChatForm() {
+    const [message, setMessage] = useState("");
+    const { setMessages } = useChat();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(message);
+
+        setMessages((prevState) => [...prevState, {message, fromMe: true}])
+        setMessage("");
+        sendMessage(message);
+    }
+
+    return ( 
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input className={styles.textInput} value={message} onChange={(e) => setMessage(e.target.value)} />
+            </form>
+        </div>
+    )
+}
+
+export default ChatForm
